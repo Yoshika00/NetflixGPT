6d752fedef27de12c225c2ec50211649d704e19d
Laxmi <laxmi18698@gmail.com>
2025-04-30 22:43:31 +0530

    Add Form Validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,22 @@
-import React, { useState } from 'react'
-import Header from './Header'
+import React, { useRef, useState } from 'react';
+import Header from './Header';
+import { checkValidData } from '../utils/validate';
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleButtonClick = () => {
+     //validate the form data
+     
+     
+     const msg = checkValidData(email?.current?.value, password?.current?.value);
+     setErrorMessage(msg);
+     
+  }
 
   const toggleSignInForm = () => {
      setIsSignInForm(!isSignInForm)
@@ -17,7 +31,7 @@ const Login = () => {
            alt=''
         />
       </div>
-      <form className='w-3/12 absolute p-12 bg-black mx-auto my-32 right-0 left-0 text-white rounded-lg bg-opacity-80'>
+      <form onSubmit={(e) => e.preventDefault()} className='w-3/12 absolute p-12 bg-black mx-auto my-32 right-0 left-0 text-white rounded-lg bg-opacity-80'>
           <h1 className='font-bold text-3xl py-4'>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
 
           {!isSignInForm && (
@@ -29,19 +43,23 @@ const Login = () => {
           )}
 
           <input 
+             ref={email}
              type='text' 
              placeholder='Email' 
              className='p-4 my-2 w-full rounded-lg bg-gray-800' 
           />
 
           <input 
+             ref={password}
              type='password' 
              placeholder='Password' 
              className='p-4 my-2 w-full rounded-lg bg-gray-800' 
           />
 
+          <p className='text-red-700 font-bold text-lg py-2'>{errorMessage}</p>
+
           <button 
-             className='p-4 my-6 bg-red-600 w-full rounded-lg'>
+             className='p-4 my-6 bg-red-600 w-full rounded-lg' onClick={handleButtonClick}>
                {isSignInForm ? "Sign In" : "Sign Up"}
           </button>
 
@@ -55,4 +73,4 @@ const Login = () => {
   )
 }
 
-export default Login
+export default Login;
