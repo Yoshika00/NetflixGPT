42652c85ca12e78326147b54a30b311cf6d62745
Laxmi <laxmi18698@gmail.com>
2025-05-03 12:29:07 +0530

    Firebase Authentication

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,8 @@
 import React, { useRef, useState } from 'react';
 import Header from './Header';
 import { checkValidData } from '../utils/validate';
+import { auth } from '../utils/firebase';
+import { createUserWithEmailAndPassword , signInWithEmailAndPassword} from 'firebase/auth';
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
@@ -12,9 +14,39 @@ const Login = () => {
   const handleButtonClick = () => {
      //validate the form data
      
-     
      const msg = checkValidData(email?.current?.value, password?.current?.value);
      setErrorMessage(msg);
+
+     if(msg) return;
+
+     if(!isSignInForm) {
+        createUserWithEmailAndPassword(auth, email.current.value, password.current.value)
+        .then((userCredential) => {
+        // Signed up 
+        const user = userCredential.user;
+        // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+           const errorMessage = error.message;
+           setErrorMessage(errorCode + "-" + errorMessage)
+         // ..
+         });
+      }
+
+     else {
+      signInWithEmailAndPassword(auth, email.current.value, password.current.value)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        setErrorMessage(errorCode + "-" + errorMessage)
+      });
+     }
      
   }
 
